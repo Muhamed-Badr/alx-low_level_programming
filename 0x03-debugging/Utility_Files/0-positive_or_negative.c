41ccfec2b358c5b541027d9767059ce8325c419c
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-02-15 14:54:05 -0800

    add files used with this project

---
+++ 0x03-debugging/Utility_Files/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - test if number stored in the variable 'n' is
+ * positive or negative or equal to 0.
+ * @i: The passed integer number
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
