0fe5a0f93125f998abb5add800864a19e539da05
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-02-21 11:56:47 -0800

    0x04-more_functions_nested_loops Advanced Tasks is Done!

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * isprime - check if n is prime
- * @n: The passed number
- *
- * Return: 1 (TRUE) if n is prime. 0 (FALSE) otherwise
- */
-short int isprime(unsigned long int n)
-{
-	unsigned long int i;
-
-	for (i = 2; (i * i) <= n; i++)
-		if (n % i == 0)
-			return (0);
-
-	if (n == 0 || n == 1)
-		return (0);
-	return (1);
-}
-
-/**
  * largest_prime_factor - finds the largest prime factor of the number num
  * @num: The passed number
  *
@@ -31,11 +12,8 @@ unsigned long int largest_prime_factor(unsigned long int num)
 	unsigned long int i;
 
 	for (i = 2; num != 1; i++)
-	{
-		if (isprime(i))
-			while (!(num % i))
-				num /= i;
-	}
+		while (!(num % i))
+			num /= i;
 
 	return (--i);
 }
