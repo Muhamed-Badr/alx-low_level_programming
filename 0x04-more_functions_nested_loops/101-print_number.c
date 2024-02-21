4a68c6c3e20c1d4f7b345e1f370517e2a4005f4e
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-02-21 13:48:28 -0800

    0x04-more_functions_nested_loops Advanced Tasks is Done!

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * number_sign - check the sign of x
+ * @x: The passed integer to check
+ *
+ * Return: 1 (TRUE) if x is positive. 0 (FALSE) otherwise
+ */
+short int number_sign(int x)
+{
+	if (x < 0)
+		return (0);
+
+	return (1);
+}
+
+/**
+ * number_digit - calculate the digit number of num
+ * @num: The passed integer number
+ *
+ * Return: The digit number of num
+ */
+short int number_digit(int num)
+{
+	short int digit_number = 0;
+
+	while (num / 10)
+	{
+		digit_number++;
+		num /= 10;
+	}
+
+	return (++digit_number);
+}
+
+/**
+ * print_number - prints an integer number
+ * @n: The passed integer to print
+ */
+void print_number(int n)
+{
+	short int i, digit = number_digit(n), mul_of_10 = 10;
+
+	if (!number_sign(n))
+	{
+		n *= (-1);
+		_putchar('-');
+	}
+
+	for (i = 2; i < digit; i++)
+		mul_of_10 *= 10;
+
+	for (i = 1; i < digit; i++)
+	{
+		_putchar((n / mul_of_10) + '0');
+		n %= mul_of_10;
+		mul_of_10 /= 10;
+	}
+
+	_putchar(n + '0');
+}
