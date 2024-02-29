472ee36c6ed777f64970a23f3449da8ac00ab1e5
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-02-29 13:48:48 -0800

    0x05-pointers_arrays_strings Advanced Tasks is Done!

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program generates random valid passwords
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int rand_num, sum = 0;
+
+	srand(time(NULL)); /* set the seed of 'rand()' function */
+
+	/* 2772 it's the number used as a condition in '101-crackme' */
+	while (sum < 2772)
+	{
+		/*
+		 * decimal numbers from 0 to 128 represent all ASCII characters
+		 */
+		rand_num = rand() % 128;
+
+		if ((sum + rand_num) > 2772)
+			break;
+
+		_putchar(rand_num);
+		sum += rand_num;
+	}
+	_putchar(2772 - sum); /* print the last character */
+	_putchar('\n');
+
+	/* printf("final sum: %d\n", sum + (2772 - sum)); */
+	/*the above printf confirms to you that the final sum will always = 2772*/
+
+	return (0);
+}
