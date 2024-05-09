f25d0479667d8c47177454e7a4027e9fa3db5661
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-05-09 16:55:31 -0700

    0x0B-malloc_free Advanced Tasks is Done!

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@ unsigned int _strlen(char *s);
  * Description: if i run program with some arguments such as
  *      `./program I will "show you" how great I am`
  *      the new string should be like:
- *      `concat_str = "I\nwill\nshow you\nhow\ngreat\nI\nam\n"`
+ *      `concat_str = "./program\nI\nwill\nshow you\nhow\ngreat\nI\nam\n"`
  *
  * Return: On success, pointer to the new concatenated string
  *         On failure OR if `ac == 0 || av == NULL`, NULL('\0')
@@ -26,22 +26,22 @@ char *argstostr(int ac, char **av)
 	 * calculate the new string length
 	 *   by sum/adding each argument length to `str_len`
 	 */
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 		str_len += _strlen(av[i]);
 	/*
 	 * after sum the length of all '\n' that will be added after each argument
-	 *   so `str_len += (ac - 1)`
+	 *   so `str_len += ac;`
 	 * then we need to add 1 to `str_len` to include the termenation null('\0')
-	 *   so `str_len += (ac - 1) + 1` = `str_len += ac`
+	 *   so `str_len += (ac + 1)`
 	 */
-	str_len += ac;
+	str_len += (ac + 1);
 
 	concat_str = malloc(sizeof(*concat_str) * str_len);
 	if (concat_str == NULL)
 		return (NULL);
 
 	/* concat all arguments in one new string */
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		/* concat each argument characters */
 		for (j = 0; av[i][j] != '\0'; j++)
