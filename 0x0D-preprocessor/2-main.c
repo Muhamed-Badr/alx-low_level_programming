3fe92a65c0b34d31f6e5ac0306a776ed294c1107
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-07-02 15:50:25 -0700

    0x0D-preprocessor Mandatory Tasks is Done!

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts(__FILE__);
