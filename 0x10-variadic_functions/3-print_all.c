c8383b85bbfe0ac2868295787ca0938536ab1aee
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-08-09 04:54:06 -0700

    0x10-variadic_functions Mandatory Tasks is Done!

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,9 +10,43 @@
 void print_all(const char * const format, ...)
 {
 	unsigned int i;
-	char c;
+	char *str, c, flag;
 	va_list ap;
 
 	va_start(ap, format);
-
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		flag = 0;
+		c = format[i];
+		switch (c)
+		{
+			case 'c':
+				flag = 1;
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				flag = 1;
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				flag = 1;
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				flag = 1;
+				str = va_arg(ap, char *);
+				if (str != NULL)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+		}
+		i++;
+		if (format[i] != '\0' && flag != 0)
+			printf(", ");
+	}
+	printf("\n");
 }
